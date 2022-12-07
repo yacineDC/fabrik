aaa9136e2e6f031a4e95cb72591fa5b6365a6cd9
yacine dh <yacine.dh@gmail.com>
2022-12-07 08:49:18 +0100

    shool

---
+++ program.py
@@ -0,0 +1,21 @@
+
+#x=55
+#y=3
+
+#var="x"
+#var="y"
+#print(la somme de str(x)+"+"+str(y)+"="+str(sum))
+#print(sum)
+#print(str(x)+"+"+str(y)+"="+str(sum))
+x=input("taper premier nombre ")
+y=input("taper deuxieme nombre ")
+s=input("taper troisiem nombre")
+#sum=int(x)+int(y)
+#sum=int(x)+int(y)
+#print(la somme de int(x)+"+"+int(y)+"="+str(sum))
+#print("la somme de "+x +"+"+y+"est :"+ str(sum))
+#sum=int(x)+int(y)
+#print("la somme de "+x+"+"+y+"est :"+str(sum))
+sum=(int(x)+int(y))/int(s)
+print("la some de " +x+"+"+y+" diviser par "+s+" est "+str(sum))
+ git init
